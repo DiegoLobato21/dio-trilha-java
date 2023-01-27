94a0824c6eb056e878acdcd70cf264b4d3b5d0f0
Diego <diego_lobato21@hotmail.com>
2023-01-27 12:02:01 -0300

    Iniciando os Estudos com Operadores.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,2 +1,15 @@
 public class TiposVariaveis {
+    public static  void main (String[]args){
+        String primeiroNome ="Diego";
+        String segundoNome ="Lobato";
+
+        System.out.println(primeiroNome.concat(" ").concat(segundoNome));
+
+        int cep = 67015200;
+
+         final long CPF= 20340592711L;
+
+        System.out.println(CPF);
+
+    }
 }
